83798bc1c1ef76e8e3978c427e94386f8ff1f674
Alena Holbert <ah72@rice.edu>
2021-01-29 00:12:28 -0600

    can play with two humans

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const DOMplayer = document.querySelector("#player-board");
+const DOMscorekeeper = document.querySelector("#scorekeeper");
 const DOMboard = document.querySelector('#gameboard');
 
 // single spot on board -- factory function
-const spot = () => {
+const spotObj = () => {
     const mark = '';
     return {
         mark
@@ -17,7 +19,7 @@ const gameboard = (() => {
     // initialize the board
     const renderBoard = () => {
         for (let i = 0; i < 9; i++) {
-            board[i] = spot();
+            board[i] = spotObj();
         };
         // board = getBoard();
         // console.log(board);
@@ -30,16 +32,8 @@ const gameboard = (() => {
             square.addEventListener('click', (e) =>
             {
                 controller.playTurn(e);
-                // console.log(e);
-                // console.log(e.target);
-                // console.log(e.target.dataset.index);
-                // console.log(board[e.target.dataset.index].mark);
-                // console.log(this);
             });
             index++;
-            // square.addEventListener('click', (e) => {
-            //     controller.playTurn();
-            // });
         });
     };
 
@@ -53,40 +47,159 @@ const gameboard = (() => {
 // module for controller that oversees the entire game
 const controller = (() => {
 
-    const player1Turn = true;
+    player1Wins = 0;
+    player2Wins = 0;
+
+    player1Turn = true;
 
     const startNewGame = () => {
+        const children = document.querySelectorAll(".spot");
+        children.forEach((child) => {
+            DOMboard.removeChild(child);
+        });
+        DOMplayer.textContent = "Player 1's Turn";
+        DOMscorekeeper.textContent = `Player 1: ${player1Wins} --- Player 2: ${player2Wins}`;
         gameboard.renderBoard();
     }
 
     // play a turn
     const playTurn = (e) => {
-        spotIndex = e.target.dataset.index;
+
+        // mark square appropriately
+        spotIndex = e.target.dataset.index; // index of DOM spot
         DOMspot = e.target;
-        spotElem = gameboard.getBoard()[spotIndex];
-        mark = spotElem.mark;
-        console.log("trtying to get the thing: " + mark);
-        if (player1Turn && mark == '') { // player 1's turn - 'X'
-            DOMspot.setAttribute('style', 'background: url(images/x-mark.png) no-repeat; background-size: 100%;');
-            spotElem.mark = "x";
-            console.log("did this update?" + spotElem.mark);
-            console.log("what about this: " + gameboard.getBoard()[spotIndex].mark);
+        spot = gameboard.getBoard()[spotIndex]; // index of internal spot object
+        markSpot(spot);
+
+        result = checkResult();
+
+        if (result != ''){
+            setTimeout(function() { // allow to render final mark
+                if (result == 'win') {
+                    if (player1Turn) {
+                        player1Wins++;
+                        if (confirm(`Player 1 Wins! Would you like to play again?`)) {
+                            startNewGame();
+                        }
+                    }
+                    else {
+                        player2Wins++;
+                        if (confirm(`Player 2 Wins! Would you like to play again?`)) {
+                            startNewGame();
+                        }
+                    }
+                }
+                else {
+                    if (confirm(`It's a Tie! Would you like to play again?`)) {
+                        startNewGame();
+                    }
+                }
+            }, 10);
         }
-        else if (mark == ''){ // player 2's turn - 'O'
-            DOMspot.setAttribute('style', 'background: url(images/o-mark.png) no-repeat; background-size: 100%;');
-            mark = "o";
+        
+        else {
+            player1Turn = !player1Turn;
+            if (player1Turn) {
+                DOMplayer.textContent = "Player 1's Turn";
+            }
+            else {
+                DOMplayer.textContent = "Player 2's Turn";
+            }
         }
     };
 
-    const test = () => {
-        console.log('uhhhh');
+    // mark spot for correct player, making sure not to override an already marked spot
+    const markSpot = (spot) => {
+
+        if (spot.mark == '') {
+
+            // player 1's turn - 'X'
+            if (player1Turn) {
+                DOMspot.setAttribute('style', 'background: url(images/x-mark.png) no-repeat; background-size: 100%;');
+                spot.mark = "x";
+                console.log("marking x");
+            }
+
+            // player 2's turn - 'O'
+            else {
+                DOMspot.setAttribute('style', 'background: url(images/o-mark.png) no-repeat; background-size: 100%;');
+                spot.mark = "o";
+                console.log("marking o");
+            }
+        }
+    };
+
+    const checkResult = () => {
+        board = gameboard.getBoard();
+        console.log(board);
+
+        console.log("win? " + checkWin(board));
+
+        // win! end game
+        if (checkWin(board)) {
+            console.log("game over! win");
+            if (player1Turn) {
+                console.log("player 1 wins!");
+            }
+            else {
+                console.log("player 2 wins!");
+            }
+            return 'win';
+        }
+
+        // tie! end game
+        else if (boardFull(board)) {
+            console.log("game over! tie");
+            return 'tie';
+        }
+
+        // game not over
+        return '';
+    };
+
+    const checkWin = (board) => {
+        
+        const winConditions = [
+            [0,1,2], // accross top
+            [3,4,5], // accross middle
+            [6,7,8], // accross bottom
+            [0,3,6], // down left
+            [1,4,7], // down middle
+            [2,5,8], // down right
+            [0,4,8], // diagonal top left to bottom right
+            [2,4,6] // diagonal bottom left to top right
+        ];
+
+        isWin = false;
+
+        winConditions.forEach((row) => {
+            if (threeInARow(board, row)) {
+                isWin = true;
+            }
+        })
+        
+        return isWin;
+    };
+
+    const threeInARow = (board, row) => {
+        return (board[row[0]].mark != '' && board[row[0]].mark == board[row[1]].mark
+            && board[row[0]].mark == board[row[2]].mark);
+    }
+
+    const boardFull = (board) => {
+        isFull = true;
+        board.forEach(spot => {
+            if (spot.mark == '') {
+                isFull = false;
+            }
+        });
+        return isFull;
     };
 
     // controller's functions accessible outside of module
     return {
         //renderBoard,
         playTurn,
-        test,
         startNewGame
     };
 })();
